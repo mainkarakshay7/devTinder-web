f3e708d3cbdc5e92566105c826394801d67a7ea0
Akshay Mainkar <mainkarakshay7@gmail.com>
2025-11-11 23:45:38 +0530

    saving chat to db

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
 
 const Chat = () => {
   const { targetUserId } = useParams();
@@ -10,6 +12,29 @@ const Chat = () => {
   const user = useSelector((store) => store.user);
   const userId = user?._id;
   const firstName = user?.firstName;
+  const lastName = user?.lastName;
+
+  const fetchChatMessages = async () => {
+    const res = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+
+    const chatMessages = res?.data?.messages?.map((msg) => {
+      const { firstName, lastName, _id } = msg?.senderId;
+      return {
+        firstName,
+        lastName,
+        text: msg.text,
+        senderId: _id,
+      };
+    });
+
+    setMessages(chatMessages);
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId) return;
@@ -17,9 +42,11 @@ const Chat = () => {
     //as soon as the page loaded, the socket connection is made and joinchat event is emitted
     socket.emit("joinChat", { firstName, userId, targetUserId });
 
-    socket.on("messageReceived", ({ firstName, text, senderId }) => {
-      console.log(firstName, text, senderId);
-      setMessages((messages) => [...messages, { firstName, text, senderId }]);
+    socket.on("messageReceived", ({ firstName, lastName, text, senderId }) => {
+      setMessages((messages) => [
+        ...messages,
+        { firstName, lastName, text, senderId },
+      ]);
     });
 
     return () => {
@@ -31,6 +58,7 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName,
+      lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -44,43 +72,27 @@ const Chat = () => {
       <div className="flex-1 overflow-scroll p-6">
         {/*Message container*/}
         {messages.map((msg, index) => (
-          <>
-            {userId === msg.senderId ? (
-              <div className="chat chat-end">
-                <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
-                    <img
-                      alt="Tailwind CSS chat bubble component"
-                      src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
-                    />
-                  </div>
-                </div>
-                <div className="chat-header">
-                  {msg.firstName}
-                  {/* <time className="text-xs opacity-50">12:46</time> */}
-                </div>
-                <div className="chat-bubble">{msg.text}</div>
-                {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
+          <div
+            className={`chat ${
+              userId === msg.senderId ? "chat-end" : "chat-start"
+            }`}
+            key={index}
+          >
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS chat bubble component"
+                  src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+                />
               </div>
-            ) : (
-              <div className="chat chat-start" key={index}>
-                <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
-                    <img
-                      alt="Tailwind CSS chat bubble component"
-                      src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-                    />
-                  </div>
-                </div>
-                <div className="chat-header">
-                  {msg.firstName}
-                  {/* <time className="text-xs opacity-50">12:45</time> */}
-                </div>
-                <div className="chat-bubble">{msg.text}</div>
-                {/* <div className="chat-footer opacity-50">Delivered</div> */}
-              </div>
-            )}
-          </>
+            </div>
+            <div className="chat-header">
+              {msg.firstName}
+              {/* <time className="text-xs opacity-50">12:46</time> */}
+            </div>
+            <div className="chat-bubble">{msg.text}</div>
+            {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
+          </div>
         ))}
       </div>
       <div className="p-5 border-b border-gray-600 flex items-center gap-2">
